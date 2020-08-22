2291edfc58d53857d3050d6194a65630fbfb5f16
Rafael Japyassu <rafa.japyassu@gmail.com>
2020-08-22 08:36:02 -0300

    Inital commit

---
+++ src/main/java/br/com/processapi/processapi/ProcessApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.processapi.processapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/processapi/processapi/ProcessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.processapi.processapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
