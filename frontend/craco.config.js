a693919dae5393fe8237deec8fba9f0bcca09d63
Rafael Japyassu <rafa.japyassu@gmail.com>
2020-08-23 19:29:24 -0300

    Configuring frontend theme

---
+++ frontend/craco.config.js
@@ -0,0 +1,21 @@
+const CracoLessPlugin = require('craco-less')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              '@primary-color': '#212121',
+              '@text-color': '#65666d',
+              '@text-color-secondary': '#9e9d9d'
+            },
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ]
+}
